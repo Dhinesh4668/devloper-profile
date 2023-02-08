7284d06000bab21428c9b494b1c49a45a34af6a0
Dhinesh Kumar <87540180+Dhinesh4668@users.noreply.github.com>
2023-02-08 22:39:38 +0530

    commit

---
+++ src/Components/Index.js
@@ -0,0 +1,7 @@
+import Nav from "./NavBar/Nav";
+import Footer from './Footer/Footer'
+
+export{
+    Nav,
+    Footer
+}
---
+++ src/screens/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+import {
+    Nav
+} from '../../Components/Index'
+
+
+const Home = () => {
+  return (
+    <div>
+        <Nav/>
+    </div>
+  )
+}
+
+export default Home
