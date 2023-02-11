300716e3a366be909197f3c4401beea82ccfc3ae
Dhinesh Kumar <87540180+Dhinesh4668@users.noreply.github.com>
2023-02-12 01:38:47 +0530

    main

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -13,15 +13,20 @@ const Home = () => {
     <div>
       <Nav />
       <Element id='home' className='home-contaoner' >
-        <div className='left-container'>
+        <div className='left-container container'>
           <div className='title-text'>
             <h1 >Hai,</h1>
             <h1 className='span'>Dhinesh Kumar</h1>
           </div>
           <div className='contant'>
-            <p>some para </p>
+            <p className='text-wrap  lh-sm-1'>I am very interested in technology and working towards it.<br /> 
+            I love to work in computers and programs.<br />
+            Now i am seeking for an entry level position <br />
+            where I can get more knowledge about them and work hard.
+            <br /> Also worked on javascript and react.js (js library) languages. 
+            </p>
           </div>
-          {/* <Button variant="outlined" color='inherit' className='btn-home'>Resume</Button> */}
+          <Button>Resume</Button>
         </div>
         <div className='right-container'>
           <img src={DevloperImage} alt='image' />
