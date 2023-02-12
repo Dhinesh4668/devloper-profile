7bc911394045a7952bfcefee941554d67c01c9b3
Dhinesh Kumar <87540180+Dhinesh4668@users.noreply.github.com>
2023-02-12 09:45:46 +0530

    main

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -21,23 +21,24 @@ const Home = () => {
   return (
     <div>
       <Nav />
-      <Element id='home' className='home-contaoner pt-5' >
-        <div className='left-container container'>
+      <Element id='home' className='home-contaoner container pt-5' >
+        <div className='left-container  col-sx-6'>
           <div className='title-text'>
             <h1 >Hai,</h1>
             <h1 className='span'>Dhinesh Kumar</h1>
           </div>
-          <div className='contant'>
-            <p className='text-wrap  lh-sm-1'>I am very interested in technology and working towards it.<br /> 
-            I love to work in computers and programs.<br />
-            Now i am seeking for an entry level position <br />
+          <div className='contant border-1'>
+            <p>
+            I am very interested in technology and working towards it. 
+            I love to work in computers and programs.
+            Now i am seeking for an entry level position 
             where I can get more knowledge about them and work hard.
-            <br /> Also worked on javascript and react.js (js library) languages. 
+            Also worked on javascript and react.js (js library) languages. 
             </p>
           </div>
           <SocialMediaIcons />
         </div>
-        <div className='right-container'>
+        <div className='right-container col-sx-1'>
           <img src={DevloperImage} alt='image' />
         </div>
       </Element>
@@ -50,8 +51,9 @@ export default Home
 
 
 function SocialMediaIcons() {
-  return<div className='icons'>
-  <a href='https://www.linkedin.com/in/dhinesh-kumar-00aa8b1a5/?originalSubdomain=in' >
+  return<div className='icons   '>
+
+    <a className='' href='https://www.linkedin.com/in/dhinesh-kumar-00aa8b1a5/?originalSubdomain=in' >
     <GrLinkedin size={"30px"} />
   </a>
   <a>
@@ -69,5 +71,7 @@ function SocialMediaIcons() {
   <a href='https://dribbble.com/dhin_designer'>
     <FiDribbble size={"30px"} />
   </a>
-  </div>
+    </div>
+  
+  
 }
